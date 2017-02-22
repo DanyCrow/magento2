e8972310bc0bf0f58994f379c57f20a2372b29b2
Nicolas KANDEL <nkandel@altima-agency.com>
2017-02-22 09:51:18 +0100

    fix cleancache

--- architecture/script/cleancache.sh
+++ architecture/script/cleancache.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 magento cache:clean
-docker exec -it git_web_1 -s /bin/bash -c "rm -rf var/di/*"
-docker exec -it git_web_1 -s /bin/bash -c "rm -rf var/generation/*"
+docker exec -it git_web_1 rm -rf /var/www/var/di
+docker exec -it git_web_1 su www-data -s /bin/bash -c "mkdir -p /var/www/var/di"
+docker exec -it git_web_1 rm -rf /var/www/var/generation
+docker exec -it git_web_1 su www-data -s /bin/bash -c "mkdir -p /var/www/var/generation"
